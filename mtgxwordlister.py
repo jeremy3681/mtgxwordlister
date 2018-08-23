52e0893045406d2c09a8f7a3c0e008f720e0c052
DESKTOP-LAIJR2B\Jeremy <jeremy3681@gmail.com>
2018-08-22 17:43:30 -0700

    Strip special chars

--- mtgxwordlister.py
+++ mtgxwordlister.py
@@ -1,17 +1,33 @@
 import json
+import re
+
+wordlist = {}
+replacedict= {u'é':'e', u'â':'a', u'á':'a', u'ö':'o', u'û':'u', u'à':'a', r' [(].*[)]':''}
 print ("Opening JSON")
-with open('AllCards-x.json', 'r') as jsonfile:
+with open('AllCards-x.json', 'r', encoding="utf-8") as jsonfile:
     cards = json.load(jsonfile)
 
-file = open('MTGXWORDLIST.txt', 'w')
-for card in cards:
-    if card.isalpha():
-        file.write(card+';'+'100\n')
 
-file.close()
+for card in cards.copy():
+    asciicard=card
+    for key in replacedict:
+        asciicard = re.sub(key,replacedict[key],asciicard)
+    cards[asciicard] = cards.pop(card)
 
-""" 
+
 for card in cards:
-    if not all(c.isalpha() or c.isspace() for c in card):
-        print(card)
- """
+        wordlist[card]='100'
+
+
+
+
+
+#remove small words from the wordlist
+for word in wordlist.copy():
+    if len(word) <3:
+        del wordlist[word]
+
+# Print the wordlist dictionary to a file
+with open('MTGXWORDLIST.txt', 'w') as listfile:
+    for word in wordlist:
+        listfile.write(word +';' + wordlist[word] + '\n')
