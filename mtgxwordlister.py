64c9fd51408629306d502673695c931fc9fa0625
DESKTOP-LAIJR2B\Jeremy <jeremy3681@gmail.com>
2018-08-23 23:02:19 -0700

    Added MTG wiki api pull

--- mtgxwordlister.py
+++ mtgxwordlister.py
@@ -1,33 +1,57 @@
 import json
 import re
+import requests
 
 wordlist = {}
-replacedict= {u'é':'e', u'â':'a', u'á':'a', u'ö':'o', u'û':'u', u'à':'a', r' [(].*[)]':''}
-print ("Opening JSON")
-with open('AllCards-x.json', 'r', encoding="utf-8") as jsonfile:
-    cards = json.load(jsonfile)
+splitlist={r' ', r'\'s ',r'-',r'_'}
+replacedict= {r'[âáà]':'a', 'é':'e', 'ö':'o', r'[ûů]':'u', 'ć':'c', 'ř':'r', 'ō':'o',
+'\n':'', r'[˝";?!.,]':'', r'[—_]':' ', r' [(].*[)]':''}
 
 
-for card in cards.copy():
-    asciicard=card
+
+def additem(item, score):
     for key in replacedict:
-        asciicard = re.sub(key,replacedict[key],asciicard)
-    cards[asciicard] = cards.pop(card)
+        item = re.sub(key,replacedict[key],item)
 
+    if not item in wordlist and 2 < len(item) < 15:
+        wordlist[item]=str(score)
 
-for card in cards:
-        wordlist[card]='100'
+    for split in splitlist:
+        subsplits = re.split(split, item)
+        for subsplit in subsplits:
+            if not subsplit in wordlist and 2 < len(subsplit) < 15:
+                wordlist[subsplit]=str(score-5)
+    
+print ("Generating MTG Crossword List")
 
+url = 'http://mtg.gamepedia.com/api.php'
+request={'action':'query', 'list':'allpages','aplimit':'max','format':'json'}
 
+lastContinue = {}
+while True:
+    req = request.copy()
+    req.update(lastContinue)
+    result = requests.get(url, params=req).json()
+    for page in result['query']['allpages']:
+        additem(page['title'],50)
+    if 'continue' not in result:
+        break
+    lastContinue = result['continue']
 
+with open("AllSets-x.json", 'r', encoding="utf-8") as jsonfile:
+    sets = json.load(jsonfile)
 
+for set in sets:
+    additem(sets[set]["code"],75)
+    additem(sets[set]["name"],100)
 
-#remove small words from the wordlist
-for word in wordlist.copy():
-    if len(word) <3:
-        del wordlist[word]
+    for card in sets[set]["cards"]:
+        additem(card["name"],100)
+        if "flavor" in card:
+            additem(card["flavor"],5)
 
 # Print the wordlist dictionary to a file
 with open('MTGXWORDLIST.txt', 'w') as listfile:
     for word in wordlist:
         listfile.write(word +';' + wordlist[word] + '\n')
+
